f03f408378f0109bb8918e51d8479951538da47d
SivaDev <42565813+siva1b3@users.noreply.github.com>
2026-04-09 18:08:08 +0000

    a phase sone

---
+++ django_app/A_phase/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from a-phase!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ django_app/B_phase/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from b-phase!")
+
+
+if __name__ == "__main__":
+    main()
