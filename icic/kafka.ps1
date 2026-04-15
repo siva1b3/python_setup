eb0afcd0c5a7022cf59cb3a880157913eb36f3e8
SivaDev <42565813+siva1b3@users.noreply.github.com>
2026-04-15 21:03:45 +0530

    Add Docker Compose configurations for ETL pipeline components and Kafka topics

---
+++ icic/kafka.ps1
@@ -0,0 +1,6 @@
+
+docker exec etl-kafka kafka-topics --bootstrap-server localhost:9092  --create --topic text-events --partitions 1 --replication-factor 1
+
+docker exec etl-kafka kafka-topics  --bootstrap-server localhost:9092  --create --topic image-events  --partitions 1  --replication-factor 1
+
+docker exec etl-kafka kafka-topics  --bootstrap-server localhost:9092  --list`
